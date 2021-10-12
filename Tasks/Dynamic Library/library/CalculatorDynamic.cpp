62ceb0c828af9e9fa79d83aed75879a9dec33a8c
Rilwan <77768891+dev-ril@users.noreply.github.com>
2021-10-12 09:23:58 +0530

    Update CalculatorDynamic.cpp

--- Tasks/Dynamic Library/library/CalculatorDynamic.cpp
+++ Tasks/Dynamic Library/library/CalculatorDynamic.cpp
@@ -1,3 +1,31 @@
 #include "pch.h"
+#include "framework.h"
+#include <utility>
+#include <limits.h>
 #include "CalculatorDynamic.h"
 
+double Calculate(double num1, double num2, int choice) 
+{
+	switch (choice)
+	{
+	case 1:
+		return num1 + num2;
+		break;
+
+	case 2:
+		return num1 - num2;
+		break;
+
+	case 3:
+		return num1 * num2;
+		break;
+
+	case 4:
+		return num1 / num2;
+		break;
+
+	default:
+		return 0.0;
+		break;
+	}
+}
